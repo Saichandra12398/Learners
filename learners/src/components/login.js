f0b1e8737b8fb2760f648c01aa25cdf6b0e737cd
charan <charan2818@msitprogram.net>
2021-06-01 16:26:53 +0530

    Added google login button

--- learners/src/components/login.js
+++ learners/src/components/login.js
@@ -2,18 +2,28 @@ import quote from '../images/quote.jpg';
 import logo from '../images/logo.png';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import GoogleLogin from 'react-google-login';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 
-function Login(){
+function Login() {
+
+    const responseSuccessGoogle = (response) => {
+        // Code for successful login
+    }
+
+    const responseFailedGoogle = (response) => {
+        // Code for failure login
+    }
+
     return (
         <div class="container-fluid" style={{ fontfamily: 'Georgia', fontsize: 35 }}>
             <div class="row">
-                <div class="col-md-1"> 
-                 {/* add logo here */}
-                 <img src={logo} width="100" /> 
-                 </div>
+                <div class="col-md-1">
+                    {/* add logo here */}
+                    <img src={logo} width="100" />
+                </div>
                 <div class="col-md-10">
                     <center>
                         <h1 id="s1">i-LMS</h1></center></div>
@@ -37,7 +47,7 @@ function Login(){
                 <div class="col-md-6" >
                     <div class="row mt-5 ml-5">
                         <center><h1>Welcome to MSIT</h1></center>
-                        </div>
+                    </div>
                     <div class="row mt-5"></div>
                     <div class="row mt-5">
                         <div class="card-border-0" style={{ backgroundcolor: 'white' }}>
@@ -46,7 +56,14 @@ function Login(){
                                     <div class="btn-group">
 
                                         <div class="first mt-5 ml-5" >
-                                         {/* add google signin button */}
+                                            {/* add google signin button */}
+                                            <GoogleLogin
+                                                clientId="268233039012-fnmuiq5has4vsi7ftttqlcj16ms204t7.apps.googleusercontent.com"
+                                                buttonText="Login with google"
+                                                onSuccess={responseSuccessGoogle}
+                                                onFailure={responseFailedGoogle}
+                                                cookiePolicy={"single_host_origin"}
+                                            />
                                         </div>
                                     </div>
                                 </div>
